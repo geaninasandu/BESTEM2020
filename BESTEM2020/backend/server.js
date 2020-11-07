060c257dc120ebaf8e478c253df54daebb73c67e
SKREFI <srazvan43@yahoo.com>
2020-11-07 13:47:16 +0200

    linked mongoose cluster

--- BESTEM2020/backend/server.js
+++ BESTEM2020/backend/server.js
@@ -7,4 +7,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+    .then(() => console.log('MongoDB database connection established successfully!'))
+    .catch(err => console.log(err));
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
