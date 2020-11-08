0554c5fa6d8e7b23e7c8472f5e3ec3932271147e
SKREFI <srazvan43@yahoo.com>
2020-11-08 10:46:59 +0200

    almost there

--- BESTEM2020/client/src/App.js
+++ BESTEM2020/client/src/App.js
@@ -28,6 +28,23 @@ function App() {
         },
     ]
 
+    let myCards2 = [
+        {
+            image_url: "https://firebasestorage.googleapis.com/v0/b/bestem-1426e.appspot.com/o/fin.png?alt=media&token=6bec115f-b7e7-48f0-93e8-9ce7ccb47525",
+            hero_name: "Fin",
+            power: "2",
+            health: "1",
+            mana: "1"
+        },
+        {
+            image_url: "https://firebasestorage.googleapis.com/v0/b/bestem-1426e.appspot.com/o/jake.png?alt=media&token=1f137568-894b-4266-8063-e25050670f55",
+            hero_name: "Jake",
+            power: "1",
+            health: "2",
+            mana: "1"
+        },
+    ]
+
     function f() {
         console.log('hello')
     }
@@ -36,20 +53,32 @@ function App() {
         <>
         <div id="enemy_hand" class="row">
             <div class="row_wraper">
-                    <Backcard /><Backcard /><Backcard />
+                <Backcard/>
+                <Backcard/>
+                <Backcard/>
+                <Backcard/>
             </div>
         </div>
 
         <div id="enemy_board" class="row">
             <div class="row_wraper">
-                    <Card /><Card /><Card /><Card />
+                {
+                    myCards.map(element => {
+                        return <Card onClick={f}
+                            image_url={element.image_url}
+                            hero_name={element.hero_name}
+                            power={element.power}
+                            health={element.health}
+                            mana={element.mana} />
+                    })
+                }
             </div>
         </div>
 
         <div id="my_board" class="row">
             <div class="row_wraper">
                 {
-                    myCards.map(element => {
+                    myCards2.map(element => {
                         return <Card onClick={f}
                             image_url={element.image_url}
                             hero_name={element.hero_name}
---
+++ BESTEM2020/client/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = (props) => {
+    return (
+        <div class="main-hero-badge">
+            
+        </div>
+    )
+}
+
+export default Hero;
